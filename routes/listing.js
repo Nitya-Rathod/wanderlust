e987a6b91532c65505f79ce9d34aca2d45f0b4a5
Nitya Rathod <nityarathod5@gmail.com>
2024-12-01 13:33:36 +0530

    committed

--- routes/listing.js
+++ routes/listing.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
-
 const listingController = require("../controller/listings.js");
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route("/")
     // Index route
     .get(wrapAsync(listingController.index)) 
     // Create Route
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)); 
+    // .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)); 
+    .post( upload.single('listing[image]'), (req,res) => {
+        res.send(req.file);
+    });
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
