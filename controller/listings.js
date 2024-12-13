bff31cd0efa021549f792b6db9bd919d27978cc5
Nitya Rathod <nityarathod5@gmail.com>
2024-12-13 12:03:08 +0530

    committed

--- controller/listings.js
+++ controller/listings.js
@@ -26,8 +26,12 @@ module.exports.showListing = async (req,res)=>{
 
 // Create
 module.exports.createListing = async (req, res)=>{
+    let url = req.file.path;
+    let filename = req.file.filename;
+    console.log(url, "---", filename);
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = { url, filename };
     await newListing.save();
     req.flash("success", "New Listing Created !");
     res.redirect("/listings");
--- routes/listing.js
+++ routes/listing.js
@@ -4,16 +4,14 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controller/listings.js");
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const { storage } = require('../cloudConfig.js');
+const upload = multer({ storage });
 
 router.route("/")
     // Index route
     .get(wrapAsync(listingController.index)) 
     // Create Route
-    // .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)); 
-    .post( upload.single('listing[image]'), (req,res) => {
-        res.send(req.file);
-    });
+    .post(isLoggedIn, upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing)); 
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
